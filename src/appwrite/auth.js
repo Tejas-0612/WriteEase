52efc17a477cb0b9e153c52117cf37b667e1b3c8
Tejas <tejashinde0612@gmail.com>
2024-03-09 12:26:15 +0530

    added authentication functionalities

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config.js";
+import { Account, Client, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Appwrite service :: createAccount :: error", error);
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("Appwrite service :: login :: error", error);
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
